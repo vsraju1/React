d29f3485787a666f5eaf8e93401026bd3e68f863
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-10-27 15:58:26 +0000

    final code

--- 02viteproject/src/main.jsx
+++ 02viteproject/src/main.jsx
@@ -2,16 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// can create directly and use the function in the same code
 function MyApp(){
   return(
     <h1>Hi there, this is a vite project and also a new function.</h1>
   )
 }
 
+
+
 // to create a new element normal way
 const anotherElement = (
   <a href='https://google.com' target='_blank'>visit google</a>
 )
+
+// react converts into this format but it is our own format react cannot read this
+const Element = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'click me to visit google'
+}
+// React has syntax to create element, look into the below code using react
 
 // to create a new element using react
 const ReactElement = React.createElement(
@@ -21,6 +35,6 @@ const ReactElement = React.createElement(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    anotherElement
+    ReactElement
   
 )
