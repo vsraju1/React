fff29a38923172d2d7d78dd15d273e75bf972abc
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-10-27 15:54:26 +0000

    way to create elements

--- 02viteproject/src/main.jsx
+++ 02viteproject/src/main.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <h1>Hi there, this is a vite project and also a new function.</h1>
+  )
+}
+
+// to create a new element normal way
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>visit google</a>
+)
+
+// to create a new element using react
+const ReactElement = React.createElement(
+  'a',
+  {'href': 'https://google.com', 'target': '_blank'},
+  'click me to vist google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    anotherElement
+  
 )
