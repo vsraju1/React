ea6e5df7f113e6f6b8efec1b48b8d0033301dadf
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-10-27 15:11:43 +0000

    created a tag

--- 03customReact/customReact.js
+++ 03customReact/customReact.js
@@ -1,3 +1,11 @@
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href' , reactElement.props.href)
+    domElement.setAttribute('target' , reactElement.props.target)
+    container.appendChild(domElement)
+}
+
 const reactElement = {
     type: 'a',
     props: {
@@ -8,4 +16,6 @@ const reactElement = {
 }
 
 const mainContainer = document.querySelector("#root")
+
+customRender(reactElement, mainContainer)
 
