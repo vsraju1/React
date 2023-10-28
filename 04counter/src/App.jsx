a9662ae6ca536bb3d88597a9afd2bb110aa1d242
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-10-28 02:35:42 +0000

    counter and usestate

---
+++ 04counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, rajuCounter] = useState(10)
+  const addValue = () => {
+    console.log('value added', counter)
+    if (counter >= 10 & counter <20) {
+      rajuCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    console.log('value removed', counter)
+    if (counter > 0) {
+      rajuCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>V Shanmukha Raju | React</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
