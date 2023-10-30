c3e46c2ee51098c98f3e33debf6e3f92bde4f68d
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-10-30 15:50:20 +0000

    final project

--- 06bgchanger_project1/src/App.jsx
+++ 06bgchanger_project1/src/App.jsx
@@ -12,27 +12,27 @@ function App() {
       <div className='flex flex-wrap justify-end gap-3 shadow-lg bg-black px-3 py-2 rounded-full'>
         <button
         onClick={() => setColor("red")} 
-        className='outline-none px-4 py-1 rounded-full text-white'
+        className='outline-none px-4 py-1 rounded-full text-white capitalize'
         style={{backgroundColor: "red"}}
         >Red</button>
         <button
         onClick={() => setColor("green")}
-        className='outline-none px-4 py-1 rounded-full text-white'
+        className='outline-none px-4 py-1 rounded-full text-white capitalize'
         style={{backgroundColor: "green"}}
         >green</button>
         <button
         onClick={() => setColor("blue")}
-        className='outline-none px-4 py-1 rounded-full text-white'
+        className='outline-none px-4 py-1 rounded-full text-white capitalize'
         style={{backgroundColor: "blue"}}
         >blue</button>
         <button 
         onClick={() => setColor("yellow")}
-        className='outline-none px-4 py-1 rounded-full text-black'
+        className='outline-none px-4 py-1 rounded-full text-black capitalize'
         style={{backgroundColor: "yellow"}}
         >yellow</button>
         <button
         onClick={() => setColor("white")}
-        className='outline-none px-4 py-1 rounded-full text-black'
+        className='outline-none px-4 py-1 rounded-full text-black capitalize'
         style={{backgroundColor: "white"}}
         >white</button>
       </div>
