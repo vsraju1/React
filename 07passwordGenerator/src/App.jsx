190a35c87127669cb28baffabb2c12648bb7edc3
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-11-01 02:37:37 +0000

    Logic added

--- 07passwordGenerator/src/App.jsx
+++ 07passwordGenerator/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
-  const [numbers, setNumbers] = useState(false)
-  const [char, setChar] = useState(false)
+  const [numberAllow, setNumberAllow] = useState(false)
+  const [charAllow, setCharAllow] = useState(false)
   const [Password, setPassword] = ("")
 
-  const passwordGenarator = () => {}
+  const passwordGenarator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllow) str += "0123456789"
+    if(charAllow) str += "!@#$%&*+=-_"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+    
+  }, [length, numberAllow, charAllow])
 
   return (
     <>
